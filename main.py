37c4b2eb8d681193a9d9fe0bafdc6c788c86774a
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2026-04-06 14:25:30 +0200

    feat: update ytdlp options

--- main.py
+++ main.py
@@ -115,7 +115,16 @@ YTDLP_OPTIONS = {
     "format": "bestaudio/best",
     "noplaylist": True,
     "quiet": True,
-    "default_search": "https://music.youtube.com/search?q=",
+    "default_search": "ytsearch",
+    "http_headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+    },
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["web"],
+        }
+    },
 }
 
 FFMPEG_OPTIONS = {
